287ec0deda89d2229fa8f52908e09403928b03c0
Christos Giannakopoulos <christos.giannakopoulos.202@gmail.com>
2025-08-08 09:11:05 -0400

    changed some color choices

--- generate_portfolio.py
+++ generate_portfolio.py
@@ -23,8 +23,8 @@ if "social_links" in data:
 
 # Set up Jinja environment
 env = Environment(loader=FileSystemLoader("."), autoescape=True)
-index_template = env.get_template("index_template.html")
-resume_template = env.get_template("resume_template.html")
+index_template = env.get_template("templates/index_template.html")
+resume_template = env.get_template("templates/resume_template.html")
 
 # Render the template with the data
 html_output = index_template.render(**data)
@@ -38,7 +38,7 @@ resume_output = resume_template.render(**data)
 with Path("index.html").open("w", encoding="utf-8") as f:
     f.write(html_output)
 
-with Path("resume.html").open("w", encoding="utf-8") as f:
+with Path("templates/resume.html").open("w", encoding="utf-8") as f:
     f.write(resume_output)
 
 print("HTML file generated successfully!")
